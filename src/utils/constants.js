10de0f7cfa57c47453fe130387cadf737c44f42d
priyanshu99ku <156918107+priyanshu99ku@users.noreply.github.com>
2025-06-15 15:42:27 +0530

    backend url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:7777";
+export const API_URL = "https://devnet-q7mg.onrender.com";
 export const DEFAULT_PROFILE_PIC = "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp";
 
 export const FEED_API = `${API_URL}/feed`;
